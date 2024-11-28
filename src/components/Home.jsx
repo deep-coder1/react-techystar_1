1ffe07c095e4b62a95a644265bbac8bb383b0a6b
deep-coder1 <ndktbd@gmail.com>
2024-11-28 08:47:44 +0530

    React TechyStar Project Work 1

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      Home
+      <main>
+        <h1>TechyStar</h1>
+        <p>Solution to all your problems</p>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
