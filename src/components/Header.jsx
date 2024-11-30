279a90f52839c81519d13a92912c86cc4eb75b65
deep-coder1 <ndktbd@gmail.com>
2024-11-30 19:54:19 +0530

    Merge error solve

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -18,23 +17,3 @@ function Header() {
 }
 
 export default Header
-=======
-import React from "react";
-import "../styles/Header.css";
-import { Link } from "react-router-dom";
-
-function Header() {
-  return (
-    <>
-      <nav>
-        {/* <a href="/">home</a> */}
-        <Link to="/">home</Link>
-        <Link to="/about">about</Link>
-        <Link to="/contact">contact</Link>
-      </nav>
-    </>
-  );
-}
-
-export default Header;
->>>>>>> 1457b1129999546ad6fcbb0313136ec428bc85ce
--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-=======
-  <App />
-);
->>>>>>> 1457b1129999546ad6fcbb0313136ec428bc85ce
