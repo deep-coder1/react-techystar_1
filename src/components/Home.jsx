85a77d43ed09c5b608db6a5b3b2bcc49d78ec1ed
deep-coder1 <ndktbd@gmail.com>
2024-12-02 15:30:51 +0530

    React TechyStar Project Work home brands section 4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import vg from "../assets/2.webp"
+import {AiFillGoogleCircle, AiFillAmazonCircle, AiFillYoutube, AiFillInstagram} from "react-icons/ai"
 
 function Home() {
   return (
@@ -24,6 +25,41 @@ function Home() {
         <div>
           <h1>Who we are?</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, ut harum! Dignissimos dolorem eveniet rem totam praesentium quisquam corporis. Ipsa eaque laudantium sint modi repellat incidunt temporibus? Quaerat iure sit fuga architecto, aspernatur quis voluptatum odit quam ullam nam nisi cupiditate deserunt eius saepe libero, sapiente labore rerum rem deleniti laudantium sed voluptates ducimus dolorem? Deleniti dolor iste dolorem deserunt ipsum, quod quia mollitia quis amet quasi nihil architecto similique!</p>
+        </div>
+      </div>
+
+      <div className="home4">
+        <div>
+          <h1>Brands</h1>
+          <article>
+            <div style={{
+              animationDelay: "0.3s",
+            }}>
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div style={{
+              animationDelay: "0.5s",
+            }}>
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div style={{
+              animationDelay: "0.7s",
+            }}>
+              <AiFillYoutube />
+              <p>YouTube</p>
+            </div>
+
+            <div style={{
+              animationDelay: "1s",
+            }}>
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
         </div>
       </div>
     </>
