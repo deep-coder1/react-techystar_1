87e535ad3ec80e165a457cb92623f51b73eccab1
deep-coder1 <ndktbd@gmail.com>
2024-12-04 21:18:18 +0530

    React TechyStar Project Work header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { HashLink } from "react-router-hash-link"
 
 function Header() {
   return (
     <nav>
         <h1>TechStar.</h1>
         <main>
-            <Link to={"/"}>Home</Link>
+            <Link to={"/#home"}>Home</Link>
             <Link to={"/contact"}>Contact</Link>
-            <Link to={"/#about"}>About</Link>
-            <Link to={"/#brands"}>Brands</Link>
+            {/* <Link to={"/#about"}>About</Link> */}
+            {/* <a href={"/#about"}>About</a> */}
+            <HashLink to={"/#about"}>About</HashLink>
+            <HashLink to={"/#brands"}>Brands</HashLink>
             <Link to={"/services"}>Services</Link>
         </main>
     </nav>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import {AiFillGoogleCircle, AiFillAmazonCircle, AiFillYoutube, AiFillInstagram} 
 function Home() {
   return (
     <>
-      <div className="home">
+      <div className="home" id="home">
         Home
         <main>
           <h1>TechyStar</h1>
@@ -21,14 +21,14 @@ function Home() {
         </div>
       </div>
 
-      <div className="home3">
+      <div className="home3" id="about">
         <div>
           <h1>Who we are?</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, ut harum! Dignissimos dolorem eveniet rem totam praesentium quisquam corporis. Ipsa eaque laudantium sint modi repellat incidunt temporibus? Quaerat iure sit fuga architecto, aspernatur quis voluptatum odit quam ullam nam nisi cupiditate deserunt eius saepe libero, sapiente labore rerum rem deleniti laudantium sed voluptates ducimus dolorem? Deleniti dolor iste dolorem deserunt ipsum, quod quia mollitia quis amet quasi nihil architecto similique!</p>
         </div>
       </div>
 
-      <div className="home4">
+      <div className="home4" id="brands">
         <div>
           <h1>Brands</h1>
           <article>
